49962cc1d154602ae96af6df67dada51c3a27483
denis sazonov <denis.sazonov@mytona.com>
2025-03-26 13:17:20 +0700

    NEXT: feat_6.c

---
+++ 2 variables/examples.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(void)
+{
+    char byte_memory;
+    byte_memory = 111;
+    char a;
+    a = byte_memory;
+
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ 2 variables/feat_5.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int d = 111;
+    double b = 550.3;
+
+    printf("%f\n", d + b);
+    return 0;
+}
+
