0aa0cda34c43aafb77859a1adbf227c03463ecb2
denis sazonov <denis.sazonov@mytona.com>
2025-04-03 15:07:11 +0700

    NEXT: feat_6

--- 2 variables/2.2 Assignment/examples.c
+++ 2 variables/2.2 Assignment/examples.c
@@ -1,1 +1,25 @@
-#NEXT
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     double d1, d2, d3, d4;
+//     float var_f;
+//     var_f = 10.0f;
+
+//     d1 = 10.0f;
+//     d2 = -7.;
+//     d3 = 1e2;
+//     d4 = 5e-3;
+
+//     int size_float = sizeof(char);
+
+//     return 0;
+
+// }
+
+int main(void)
+{
+    short d;
+    d = 5;
+    return 0;
+}
---
+++ 2 variables/2.2 Assignment/feat_3.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+    int a,b,c,d;
+
+    c = 5; d = c;
+    a = b = -23;
+    a = 100;
+    
+    return 0;
+}
