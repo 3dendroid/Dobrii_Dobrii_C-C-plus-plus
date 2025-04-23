1982bc302b21695489ede673f90d47591410f0d7
denis sazonov <denis.sazonov@mytona.com>
2025-04-23 11:29:12 +0700

    NEXT: feat_2.c

--- 2 variables/2.4 printf/examples.c
+++ 2 variables/2.4 printf/examples.c
@@ -1,1 +1,17 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    // int var_i = 789;
+    // short var_h = 100;
+    // long double var_ld = 0.5;
+    //long long ll
+    //log double L
+    //char, short
+    int var_i = -12425;
+    double var_d = -40.58913;
+    printf("[%X]\n", var_i);
+    printf("[%+.2f]\n", var_d);
+    // printf("var_h = %hd, var_i = %d, var_ld = %Lf", var_h, var_i, var_ld);
+    return 0;
+}
