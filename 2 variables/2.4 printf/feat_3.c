da7062e52e1a0b80da03b32ad280b4e4b54a4237
denis sazonov <denis.sazonov@mytona.com>
2025-05-01 14:58:10 +0700

    NEXT: feat_5.c

--- 2 variables/2.4 printf/feat_3.c
+++ 2 variables/2.4 printf/feat_3.c
@@ -1,1 +1,14 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    char byte = 65;
+    unsigned short height = 1055;
+    int distance = 1000000;
+    float pi = 3.1415f;
+
+    // здесь продолжайте программу
+    printf("%c\n%u\n%d\n%f\n", byte, height, distance, pi);
+
+    return 0;
+}
