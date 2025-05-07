c29e004b59abbc37e311cecc81bc81c457104695
denis sazonov <denis.sazonov@mytona.com>
2025-05-07 12:34:21 +0700

    NEXT: folder 2.5

---
+++ 2 variables/2.4 printf/feat_10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned char h = 17, m = 45, s = 5;
+
+    // здесь продолжайте программу
+    printf("0x%02x:0x%02x:0x%02x", h, m, s);
+    // printf("%#.2x:%#.2x:%#.2x", h, m, s);
+
+    return 0;
+}
--- 2 variables/2.4 printf/feat_9.c
+++ 2 variables/2.4 printf/feat_9.c
@@ -1,1 +1,11 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned char h = 11, m = 8, s = 1;
+
+    printf("%02d:%02d:%02d\n", h, m, s);
+    printf("%d:0%d:0%d", h, m, s);
+
+    return 0;
+}
