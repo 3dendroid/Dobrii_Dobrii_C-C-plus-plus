f2217aff68e5d82e7ed9e207c594254eaf70936b
denis sazonov <denis.sazonov@mytona.com>
2025-05-09 17:53:48 +0700

    NEXT: feat_1.c

--- 2 variables/2.5 scanf/examples.c
+++ 2 variables/2.5 scanf/examples.c
@@ -1,1 +1,14 @@
-# NEXT
+#include <stdio.h>
+
+
+int main(void)
+{   
+    unsigned int price = 0;
+    double weight = 0.0;
+
+    int res = scanf("%*llu; %u; %f", &price, &weight);
+
+    printf("res = %d: price = %d, weight = %.2f", res, price, weight);
+
+    return 0;
+}
