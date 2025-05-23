97cbe7c0c29ed1b96e4656b9908473ca0396faff
denis sazonov <denis.sazonov@mytona.com>
2025-05-23 17:06:10 +0700

    NEXT: feat_1.c

--- 2 variables/2.6 math_operations/examples.c
+++ 2 variables/2.6 math_operations/examples.c
@@ -1,1 +1,21 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    short a = -5;
+    int b = 10;
+    float c = 5.4f;
+    double d = -5.4;
+
+    // int res_1 = a + b;
+    // short res_2 = 100 - b;
+    // float res_3 = 5.9 - c;
+    // double res_4 = d * 3;
+
+    int res_1 = 7 / 2;
+    short res_2 = -9 / b;
+    float res_3 = a / c;
+    double res_4 = d / b;
+
+    return 0;
+}
