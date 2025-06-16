be766b8438ff6b7fd331568f0318e700b94c7ac3
denis sazonov <denis.sazonov@mytona.com>
2025-06-16 17:09:56 +0700

    NEXT: feat_5.c

--- 2 variables/2.7 del_operations/feat_4.c
+++ 2 variables/2.7 del_operations/feat_4.c
@@ -2,15 +2,25 @@
 
 int main(void)
 {
-    unsigned short time_h = 10; // часы
-    unsigned short time_m = 33; // минуты
-    unsigned short time_s = 5; // секунды
-
-    unsigned short h, m, s; // добавляемое время: h - часы; m - минуты; s - секунды
+    unsigned short time_h = 10, time_m = 33, time_s = 5;
+    unsigned short h, m, s;
     scanf("%hu; %hu; %hu", &h, &m, &s);
 
-    // здесь продолжайте программу
-    # NEXT
-    
+    // 1) Сложение секунд и перенос в минуты
+    unsigned short sum_s = time_s + s;
+    unsigned short res_s = sum_s % 60;
+    unsigned short carry_m = sum_s / 60;
+
+    // 2) Сложение минут и перенос в часы
+    unsigned short sum_m = time_m + m + carry_m;
+    unsigned short res_m = sum_m % 60;
+    unsigned short carry_h = sum_m / 60;
+
+    // 3) Сложение часов и закрытие суток
+    unsigned short sum_h = time_h + h + carry_h;
+    unsigned short res_h = sum_h % 24;
+
+    // 4) Вывод в формате hh:mm:ss
+    printf("%02hu:%02hu:%02hu\n", res_h, res_m, res_s);
     return 0;
 }
