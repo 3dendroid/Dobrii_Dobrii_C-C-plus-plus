3dca2f9636a2c4ea28be1a89e09125360728fbbd
denis sazonov <denis.sazonov@mytona.com>
2025-06-20 14:18:06 +0700

    NEXT: folder 2.8

--- 2 variables/2.7 del_operations/feat_8.c
+++ 2 variables/2.7 del_operations/feat_8.c
@@ -1,1 +1,12 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    float x;
+    // читаем вещественное число
+    if (scanf("%f", &x) != 1)
+        return 0;
+    // в printf сразу уменьшаем x на 1 и выводим с точностью до десятых
+    printf("%.1f\n", x -= 1);
+    return 0;
+}
