d257daff999827630f13ed85d41eda28ba29d1ab
Denis Sazonov <denis.sazonov@mytona.com>
2025-09-16 12:24:52 +0700

    NEXT: feat_2.c

---
+++ 3 bool_types/3.2 if_operator/feat_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    short x, y;
+    if(scanf("%hd, %hd", &x, &y) != 2) {
+        printf("Input error.");
+        return 0;
+    }
+
+    // здесь продолжайте программу
+
+    return 0;
+}
