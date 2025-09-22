8a189a40d424090efba9d1bbbea18b4ebdb3c2ad
denis.sazonov <denis.sazonov@mytona.com>
2025-09-22 12:20:50 +0700

    NEXT: feat_5.c

--- 3 bool_types/3.2 if_operator/feat_4.c
+++ 3 bool_types/3.2 if_operator/feat_4.c
@@ -1,1 +1,21 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a == b)
+    {
+        printf("%d", b);
+    }
+    else if (a <= b)
+    {
+        printf("%d %d", a, b);
+    }
+    else
+    {
+        printf("%d %d", b, a);
+    }
+    return 0;
+}
