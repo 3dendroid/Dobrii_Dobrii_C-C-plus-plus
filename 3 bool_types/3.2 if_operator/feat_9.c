2d106411872b2b1c33a58c79b8b136573181e870
denis.sazonov <denis.sazonov@mytona.com>
2025-10-06 13:14:09 +0700

    NEXT: folder 3.3

--- 3 bool_types/3.2 if_operator/feat_9.c
+++ 3 bool_types/3.2 if_operator/feat_9.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    float a, b, c;
+    scanf("%f, %f, %f", &a, &b, &c);
+
+    float min = a;
+    if (b < min)
+        min = b;
+    if (c < min)
+        min = c;
+
+    printf("%.2f", min);
+    return 0;
+}
