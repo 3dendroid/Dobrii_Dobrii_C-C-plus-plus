4e0a847175ae8c1fb5eb10969e6d996789b525e2
denis.sazonov <denis.sazonov@mytona.com>
2025-10-14 13:13:18 +0700

    NEXT: feat_6.c

--- 3 bool_types/3.3 tern_operators/feat_5.c
+++ 3 bool_types/3.3 tern_operators/feat_5.c
@@ -1,1 +1,11 @@
-# NEXT
+#include <stdio.h>
+
+int main() {
+    int width, height;
+    scanf("%d %d", &width, &height);
+
+    int border = (width > height ? width : height) + 8;
+
+    printf("%d", border);
+    return 0;
+}
