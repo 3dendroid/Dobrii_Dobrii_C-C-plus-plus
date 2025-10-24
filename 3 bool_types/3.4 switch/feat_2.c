0efa0eb35587d148ad4adc224a73512ccc4611dd
denis.sazonov <denis.sazonov@mytona.com>
2025-10-24 13:42:08 +0700

    NEXT: feat_3.c

--- 3 bool_types/3.4 switch/feat_2.c
+++ 3 bool_types/3.4 switch/feat_2.c
@@ -1,1 +1,39 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    char symbol;
+    if (scanf("%c", &symbol) != 1)
+    {
+        printf("Input error.");
+        return 0;
+    }
+
+    switch (symbol)
+    {
+    case 'a':
+        symbol = 'A';
+        break;
+    case 'b':
+        symbol = 'B';
+        break;
+    case 'c':
+        symbol = 'C';
+        break;
+    case 'd':
+        symbol = 'D';
+        break;
+    case 'e':
+        symbol = 'E';
+        break;
+    case 'f':
+        symbol = 'F';
+        break;
+    default:
+        break;
+    };
+
+    printf("%c", symbol);
+    __ASSERT_TESTS__
+    return 0;
+}
