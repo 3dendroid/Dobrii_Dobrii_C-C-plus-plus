6cece05ee503dfca801b875938852f3e300580e7
denis.sazonov <denis.sazonov@mytona.com>
2025-11-07 10:52:41 +0700

    NEXT: feat_6.c

--- 3 bool_types/3.5 bit_operations/feat_5.c
+++ 3 bool_types/3.5 bit_operations/feat_5.c
@@ -5,7 +5,10 @@ int main(void)
     unsigned char bits;
     scanf("%hhu", &bits);
 
-    // здесь продолжайте программу
+    unsigned char mask = ~(1 << 7 | 1 << 6); // маска с нулями в 7 и 6 битах
+    unsigned char res = bits & mask;         // выключаем нужные биты
+
+    printf("%d\n", res);
 
     return 0;
 }
