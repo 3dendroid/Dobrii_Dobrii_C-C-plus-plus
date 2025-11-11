8c5999c57d6c942b969a1b206e0a45ae8eca68ae
denis.sazonov <denis.sazonov@mytona.com>
2025-11-11 13:10:11 +0700

    NEXT: feat_7.c

--- 3 bool_types/3.5 bit_operations/feat_6.c
+++ 3 bool_types/3.5 bit_operations/feat_6.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned char bits;
+    scanf("%hhu", &bits);
+
+
+    unsigned char mask = (1 << 4) | (1 << 2) | (1 << 0);
+
+    unsigned char res = bits | mask;
+
+    printf("%d\n", res);
+
+    return 0;
+}
