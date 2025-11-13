d2a5d841f8b75bd897bf014193623033cff0eba8
3dendroid <denkingbad@gmail.com>
2025-11-13 13:55:51 +0700

    NEXT: feat_8.c

--- 3 bool_types/3.5 bit_operations/feat_7.c
+++ 3 bool_types/3.5 bit_operations/feat_7.c
@@ -1,1 +1,15 @@
-# NEXT3
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned char bits;
+    scanf("%hhu", &bits);
+
+    unsigned char mask = (1 << 3) | (1 << 0);
+
+    unsigned char res = bits ^ mask;
+
+    printf("%d\n", res);
+
+    return 0;
+}
