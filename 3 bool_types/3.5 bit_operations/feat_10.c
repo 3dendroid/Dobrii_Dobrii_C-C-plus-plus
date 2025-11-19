aff9a56af4c81fbcc3aa41fb1aed75d470a99285
3dendroid <denkingbad@gmail.com>
2025-11-19 10:52:21 +0700

    NEXT: feat_11.c

--- 3 bool_types/3.5 bit_operations/feat_10.c
+++ 3 bool_types/3.5 bit_operations/feat_10.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int a, b;
+    scanf("%u %u", &a, &b);
+
+    a >>= 1;
+    b >>= 1;
+
+    unsigned int res = a * b;
+
+    printf("%u", res);
+
+    return 0;
+}
