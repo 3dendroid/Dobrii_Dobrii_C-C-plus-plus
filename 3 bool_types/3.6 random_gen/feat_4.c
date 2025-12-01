6944ffab809d4722d49d6a7ba6719550e1ab0498
3dendroid <denkingbad@gmail.com>
2025-12-01 15:37:45 +0700

    NEXT: feat_5.c

--- 3 bool_types/3.6 random_gen/feat_4.c
+++ 3 bool_types/3.6 random_gen/feat_4.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    for (int i = 0; i < 10; i++) {
+        int rnd = rand();
+        int y = rnd % (b - a + 1) + a;
+        printf("%d ", y);
+    }
+
+    return 0;
+}
