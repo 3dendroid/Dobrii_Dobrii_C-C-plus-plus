cc0cfd7d1da6c1f3ee6344f4b682f8b14caed264
3dendroid <denkingbad@gmail.com>
2025-12-03 12:30:59 +0700

    NEXT: feat_6.c

--- 3 bool_types/3.6 random_gen/feat_5.c
+++ 3 bool_types/3.6 random_gen/feat_5.c
@@ -1,1 +1,17 @@
-# NEXT
+#include <stdio.h>
+#include <stdlib.h>
+#define RAND_MAX 32767
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    for (int i = 0; i < 10; i++) {
+        int rnd = rand();
+        double y = (double)rnd / RAND_MAX * (b - a) + a;
+        printf("%.2f ", y);
+    }
+
+    return 0;
+}
