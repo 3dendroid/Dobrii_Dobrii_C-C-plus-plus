5c3a982eee26970c0dd138f5ad241755530a2754
3dendroid <denkingbad@gmail.com>
2025-12-05 15:32:15 +0700

    NEXT: feat_10.c

---
+++ 3 bool_types/3.6 random_gen/feat_10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned short N;
+    scanf("%hu", &N);
+
+    // здесь продолжайте программу
+
+    return 0;
+}
--- 3 bool_types/3.6 random_gen/feat_6.c
+++ 3 bool_types/3.6 random_gen/feat_6.c
@@ -1,1 +1,11 @@
-# NEXT
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    r1, r2, r3, r4, r5;
+
+    __ASSERT_TESTS__ // макроопределение для тестирования (не убирать и должно идти непосредственно перед return 0)
+    return 0;
+}
