6402b8ab1c1e9ba3157f0b2a4679ac8417dcd88f
3dendroid <denkingbad@gmail.com>
2025-12-09 15:42:08 +0700

    NEXT: feat_11.c

--- 3 bool_types/3.6 random_gen/feat_10.c
+++ 3 bool_types/3.6 random_gen/feat_10.c
@@ -5,7 +5,10 @@ int main(void)
     unsigned short N;
     scanf("%hu", &N);
 
-    // здесь продолжайте программу
+    unsigned short total = N * 7;
+    unsigned short packs = (total + 99) / 100;
+
+    printf("%hu", packs);
 
     return 0;
 }
