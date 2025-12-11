8852c0cbbdfc132b9b02a9931520717b905ec64e
3dendroid <denkingbad@gmail.com>
2025-12-11 19:55:41 +0700

    NEXT: feat_12.c

--- 3 bool_types/3.6 random_gen/feat_11.c
+++ 3 bool_types/3.6 random_gen/feat_11.c
@@ -1,1 +1,13 @@
-# NEXT
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    unsigned short N;
+    scanf("%hu", &N);
+
+    double cm = N * 2.54;
+    printf("%d", (int)round(cm));
+
+    return 0;
+}
