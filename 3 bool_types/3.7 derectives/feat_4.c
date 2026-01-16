15918d66b5bf4c63ae751f1084aaefbebaace3f8
3dendroid <denkingbad@gmail.com>
2026-01-16 10:31:10 +0100

    NEXT: feat_5.c

--- 3 bool_types/3.7 derectives/feat_4.c
+++ 3 bool_types/3.7 derectives/feat_4.c
@@ -1,1 +1,14 @@
-# NEXT
+#include <stdio.h>
+
+#define KMH (3600.0 / 1000.0)
+
+int main(void)
+{
+    int s_mc;
+    scanf("%d", &s_mc);
+
+    double s_kmh = s_mc * KMH;
+    printf("%.2f", s_kmh);
+
+    return 0;
+}
