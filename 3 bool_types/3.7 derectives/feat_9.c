e99402e9f7003755f666d818eb6e4502b4445c7f
3dendroid <denkingbad@gmail.com>
2026-01-26 08:30:28 +0100

    NEXT: folder 3.8

--- 3 bool_types/3.7 derectives/feat_9.c
+++ 3 bool_types/3.7 derectives/feat_9.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+#define MAX_BUFF_SIZE 1024
+
+int main(void)
+{
+    int buff_size;
+
+    scanf("%d", &buff_size);
+
+    buff_size = (buff_size > MAX_BUFF_SIZE) ? MAX_BUFF_SIZE : buff_size;
+
+    printf("%d", buff_size);
+
+    return 0;
+}
