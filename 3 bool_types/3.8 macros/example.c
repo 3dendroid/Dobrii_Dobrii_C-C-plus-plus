7d87a9e0a6e76de0dd0e74970d9605679553a1f8
3dendroid <denkingbad@gmail.com>
2026-01-28 11:03:20 +0100

    NEXT: feat_1.c

--- 3 bool_types/3.8 macros/example.c
+++ 3 bool_types/3.8 macros/example.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+#define SQ_PR(A, B) ((A) * (B))
+#define TEXT(A, B) "Square of rectangle (" #A ") x (" #B ")\n"
+#define X_N(N) x##N
+
+int main(void)
+{
+    int x1 = 1, x2 = 2, x4 = 10;
+    printf("%d\n", X_N(4));
+
+    // int res = SQ_PR(2,3);
+    // printf(TEXT(x + 2, y - 3));
+
+    return 0;
+}
