7c046c37f44b4efd6eb05d0cc1654ff56e371f97
3dendroid <denkingbad@gmail.com>
2026-02-09 10:36:58 +0100

    NEXT: feat_3.c

--- 3 bool_types/3.9 include/example.c
+++ 3 bool_types/3.9 include/example.c
@@ -1,1 +1,19 @@
-# NEXT
+// #include <stdio.h>
+#define LANG_C
+
+#if defined(LANG_C)
+#include <stdio.h>
+#else
+#include <iostream>
+#endif
+
+int main()
+{
+    int x = 5;
+#ifdef LANG_C
+    printf("%d\n", x);
+#else
+    std::cout << x << std::endl;
+#endif
+    return 0;
+}
