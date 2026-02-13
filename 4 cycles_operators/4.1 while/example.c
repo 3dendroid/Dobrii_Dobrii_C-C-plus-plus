80eb839546d7f8f2189d53bc4e7bbc1a8fe75998
3dendroid <denkingbad@gmail.com>
2026-02-13 10:43:37 +0100

    NEXT: feat_3.c

--- 4 cycles_operators/4.1 while/example.c
+++ 4 cycles_operators/4.1 while/example.c
@@ -1,1 +1,19 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int s = 0;
+    int x;
+
+    while (scanf("%d", &x) == 1 && x != 0)
+    {
+        res = x % 2;
+        if (res == 0)
+            s += x;
+    }
+
+    printf("res = %d\n", res);
+    printf("s = %d\n", s);
+
+    return 0;
+}
---
+++ 4 cycles_operators/4.1 while/feat_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int count = 0;
+    int s = 0;
+
+    while (count++ <= 5)
+        s += count;
+
+    printf("s = %d\n", s);
+
+    return 0;
+}
