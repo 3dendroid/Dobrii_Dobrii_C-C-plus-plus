f8feb171545cf9d614a01a567c2ec4cafad5077a
3dendroid <denkingbad@gmail.com>
2026-03-03 18:40:56 +0100

    NEXT: feat_7.c

--- 4 cycles_operators/4.2 for/feat_4.c
+++ 4 cycles_operators/4.2 for/feat_4.c
@@ -1,1 +1,20 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int b1, q, n;
+    scanf("%d; %d; %d", &b1, &q, &n);
+
+    int val = b1;
+
+for (int i = 0; i < n; i++)
+{
+    printf("%d", val);
+    if (i < n - 1)
+        printf(" ");
+
+    val *= q;
+}
+
+    return 0;
+}
