58ea4f7952132cd5e2c6a116945c2219545fa312
3dendroid <denkingbad@gmail.com>
2026-03-11 11:52:46 +0100

    NEXT: feat_3.c

--- 4 cycles_operators/4.3 do while/example.c
+++ 4 cycles_operators/4.3 do while/example.c
@@ -1,1 +1,15 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int total_boxes = 3;
+    int total_files = 6;
+
+    for (int i = 0; i < total_boxes; ++i)
+        for (int j = 0; j < total_files; ++j)
+        {
+            printf("Box %d, File %d\n", i + 1, j + 1);
+        }
+
+    return 0;
+}
