7348f8588627b876f3ad017f5c625ac1c35cf83b
SUPERDEN <denkingbad@gmail.com>
2026-05-22 10:41:28 +0200

    NEXT: folder 5.4

--- 5 arrays_strings/5.3 coursors/feat_7.c
+++ 5 arrays_strings/5.3 coursors/feat_7.c
@@ -1,1 +1,18 @@
-# NEXT, check
+#include <stdio.h>
+
+int main(void)
+{
+    double arr[20];
+    int n = 0;
+    double sum = 0;
+
+    while (scanf("%lf", &arr[n]) == 1)
+    {
+        sum += arr[n];
+        n++;
+    }
+
+    printf("%.2lf", sum / n);
+
+    return 0;
+}
--- 5 arrays_strings/5.3 coursors/feat_8.c
+++ 5 arrays_strings/5.3 coursors/feat_8.c
@@ -1,1 +1,25 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    short var_1;
+    int var_2;
+    long long var_3;
+
+    scanf("%hd, %d, %lld", &var_1, &var_2, &var_3);
+
+    char *ptr1 = (char *)&var_1;
+    char *ptr2 = (char *)&var_2;
+    char *ptr3 = (char *)&var_3;
+
+    for (int i = 0; i < sizeof(var_1); i++)
+        printf("%u ", (unsigned char)ptr1[i]);
+
+    for (int i = 0; i < sizeof(var_2); i++)
+        printf("%u ", (unsigned char)ptr2[i]);
+
+    for (int i = 0; i < sizeof(var_3); i++)
+        printf("%u ", (unsigned char)ptr3[i]);
+
+    return 0;
+}
