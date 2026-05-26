79eb0d1c29342b65b51a8936a2c7b49f8288f971
SUPERDEN <denkingbad@gmail.com>
2026-05-26 09:15:07 +0200

    NEXT: feat_1.c

--- 5 arrays_strings/5.4 const/example.c
+++ 5 arrays_strings/5.4 const/example.c
@@ -1,1 +1,14 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+
+    short ar[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const short *ptr_ar;
+
+    //ptr_ar = ar;
+    short x = ptr_ar[0];
+    //ptr_ar++;
+
+    return 0;
+}
