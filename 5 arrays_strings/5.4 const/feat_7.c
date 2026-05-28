3f346dcdc0789b846807ad4f0130091b65e0365c
SUPERDEN <denkingbad@gmail.com>
2026-05-28 12:39:14 +0200

    NEXT: folder 5.5

---
+++ 5 arrays_strings/5.4 const/feat_7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define CELL_N(N) cell_##N
+
+int main(void)
+{
+    short cell_1 = 1, cell_2 = 2, cell_3 = 3;
+    int n = 2;
+
+    int res_1 = CELL_N(1) + 10;
+    // int res_2 = CELL_N(n) * 2;
+    int res_3 = CELL_N(1 + 2) * 3;
+
+    return 0;
+}
