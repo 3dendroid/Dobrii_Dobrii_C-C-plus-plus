df70064201819628ccc44507eda0dcc12390ab31
SUPERDEN <denkingbad@gmail.com>
2026-06-25 10:06:05 +0200

    NEXT: feat_3.c

--- 5 arrays_strings/5.8 input/example.c
+++ 5 arrays_strings/5.8 input/example.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+int main()
+{
+    // char sp[] = "Hello, World!";
+    // // printf("sp = %s\n", sp);
+    // puts(sp);
+    // puts(sp);
+
+    char bf[10];
+    // scanf("%9s", bf);
+    fgets(bf, sizeof(bf), stdin);
+    puts(bf);
+
+    return 0;
+}
