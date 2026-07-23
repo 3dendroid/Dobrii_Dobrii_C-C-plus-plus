6d87ddea14ae2a6f8312f5fe8d6b2e9209bbba3d
SUPERDEN <denkingbad@gmail.com>
2026-07-23 12:20:42 +0200

    NEXT: feat_6.c

--- 6 functions/6.2 return/feat_4.c
+++ 6 functions/6.2 return/feat_4.c
@@ -1,1 +1,16 @@
-# NEXT
+#include <stdio.h>
+
+double get_mean2(int a, int b)
+{
+    return (a + b) / 2.0;
+}
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%.1f\n", get_mean2(a, b));
+
+    return 0;
+}
